8b99ddfcc723f1b37c1aeab7b86efaf666727c2a
Shaurya Saria <sariashaurya09@gmail.com>
2026-02-28 17:24:53 +0530

    solve 71A solution code - C++

---
+++ problems/800/71A_WayTooLongWords_CF.cpp
@@ -0,0 +1,24 @@
+// Codeforces 71A - Way Too Long Words (800)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    if (!(cin >> n)) return 0;
+
+    while (n--) {
+        string s;
+        cin >> s;
+        if (s.size() > 10) {
+            cout << s.front() << (s.size() - 2) << s.back() << '\n';
+        } else {
+            cout << s << '\n';
+        }
+    }
+
+    return 0;
+}
+

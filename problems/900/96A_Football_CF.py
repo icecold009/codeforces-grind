a7c6b5046ec81431db3be1903609b3451aa53eb5
Shaurya Saria <sariashaurya09@gmail.com>
2026-03-04 17:55:52 +0530

    Add Python solution for 96A - Football (900)

---
+++ problems/900/96A_Football_CF.py
@@ -0,0 +1,14 @@
+# Codeforces 96A - Football (900)
+
+
+def main():
+    s = input().strip()
+    if "0000000" in s or "1111111" in s:
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == "__main__":
+    main()
+

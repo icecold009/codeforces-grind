a204226260b1415ee144670624db9af0f3de62de
Shaurya Saria <sariashaurya09@gmail.com>
2026-03-14 23:10:51 +0530

    Add 231A Team solution in Java

---
+++ problems/800/231A_Team_CF.java
@@ -0,0 +1,28 @@
+// Codeforces 231A - Team (800)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+        int solved = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a + b + c >= 2) {
+                solved++;
+            }
+        }
+
+        System.out.println(solved);
+    }
+}
+
